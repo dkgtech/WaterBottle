767230f9e14454d7befc5d02f1168a64107ed51c
Dipesh Kumar Gupta <144589068+dkgtech@users.noreply.github.com>
2025-02-16 15:29:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Show button when scrolling down
+window.onscroll = function () {
+    let btn = document.getElementById("scrollTopBtn");
+    if (document.documentElement.scrollTop > 100) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+};
+
+// Scroll to top function
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
